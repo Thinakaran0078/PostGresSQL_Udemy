c0c97d86baa242ffe4374f8cbfa9fce48606efbd
Dhina <thinakaran0078@gmail.com>
2025-08-29 14:58:54 +0530

    final code

---
+++ database.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+connection = sqlite3.connect("data_postgres.db")
+connection.row_factory = sqlite3.Row
+
+def create_table():
+    with connection:
+         connection.execute(
+             '''CREATE TABLE IF NOT EXISTS entries (content TEXT, date TEXT)'''
+             )
+
+
+def add_entry(entry_content, entry_date):
+    with connection:
+        connection.execute(
+            "INSERT INTO entries (content, date) VALUES (?, ?)", (entry_content, entry_date)
+            )
+
+def get_entries():
+    return connection.execute("SELECT * FROM entries;")
